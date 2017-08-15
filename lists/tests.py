9c424c46c8172daf63c4b477cb86505219cd7f08
saraducks <saranyadhakshina25@gmail.com>
2017-08-14 19:54:26 -0700

    failed the view mappings

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
    def test_math(self):
-       self.assertEqual(2,7-3)
+       found = resolve("/")
+       self.assertEqual(found.func, home_page)
 
