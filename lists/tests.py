b2e67d3f4396ff003e644d46e427a7256e51bd01
saraducks <saranyadhakshina25@gmail.com>
2017-08-15 23:10:45 -0700

    assertTemplateUsed instead of adding manual tags

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -11,10 +12,11 @@ class SmokeTest(TestCase):
        self.assertEqual(found.func, home_page)
   
    def test_home_page(self):
-       request = HttpRequest()
-       response = home_page(request)
-       html = response.content.decode('utf-8')
-       self.assertTrue(html.startswith('<html>'))
-       self.assertIn('<title>To-Do lists</title>', html)
-       self.assertTrue(html.strip().endswith('</html>')) 
-       
+       #request = HttpRequest()
+       #response = home_page(request)
+       #html = response.content.decode('utf-8')
+       #result = render_to_string('home.html')
+       #self.assertEqual(html, result)
+       response = self.client.get('/')
+       self.assertTemplateUsed(response, 'home.html')
+
