8c21ff724331d46929e8f26124b38e72631a91ff
saraducks <saranyadhakshina25@gmail.com>
2017-08-15 20:50:41 -0700

    Request to homage_page failed as expected

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -8,4 +9,12 @@ class SmokeTest(TestCase):
    def test_math(self):
        found = resolve("/")
        self.assertEqual(found.func, home_page)
-
+  
+   def test_home_page(self):
+       request = HttpRequest()
+       response = home_page(request)
+       html = response.content.decode('utf-8')
+       self.assertTrue(html.startswith('<html>'))
+       self.assertIn('<title>To-Do lists</title>', html)
+       self.assertTrue(html.endswith('</html>')) 
+       
